7cc7524ca92acfe450bbf16a3584ef1e606057be
Dan Ni <61080530+Danni4real@users.noreply.github.com>
2026-07-17 17:09:41 +0800

    Refactor loop syntax and add threading with sleep

--- example.cpp
+++ example.cpp
@@ -1,4 +1,5 @@
 #include <cmath>
+#include <unistd.h>
 #include <sys/mman.h>
 
 #include "profiler.h"
@@ -10,12 +11,14 @@ double cpu_heavy() {
     double x{};
     static int i{};
 
-    for (int c=0;c<1000;c++) {
+    for (int c = 0; c < 1000; c++) {
         i++;
 
         x += sin(i) * cos(sqrt(i + 1));
         x = fmod(x, 1000.0);
     }
+
+    usleep(1);
 
     return x;
 }
@@ -25,9 +28,11 @@ void syscall_heavy() {
     // std::cout << "syscall_heavy\n";
 
     timespec ts{};
-    for (int c=0;c<1000;c++) {
+    for (int c = 0; c < 1000; c++) {
         clock_gettime(CLOCK_MONOTONIC, &ts);
     }
+
+    usleep(1);
 }
 
 void mmap_heavy() {
@@ -36,20 +41,37 @@ void mmap_heavy() {
 
     const size_t sz = 4096 * 10;
 
-    for (int c=0;c<1000;c++) {
+    for (int c = 0; c < 1000; c++) {
         void* p = mmap(nullptr, sz, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
         if (p != MAP_FAILED) {
             munmap(p, sz);
         }
     }
+
+    usleep(1);
 }
 
 int main() {
+    std::thread([] {
+        while (true) {
+            cpu_heavy();
+        }
+    }).detach();
+
+    std::thread([] {
+        while (true) {
+            syscall_heavy();
+        }
+    }).detach();
+
+    std::thread([] {
+        while (true) {
+            mmap_heavy();
+        }
+    }).detach();
 
     while (true) {
-        cpu_heavy();
-        syscall_heavy();
-        mmap_heavy();
+        sleep(60);
     }
 
     return 0;
