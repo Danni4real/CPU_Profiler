069ac6717be80bcecba5a197aee3368c917a8b29
Dan Ni <61080530+Danni4real@users.noreply.github.com>
2026-07-17 16:00:07 +0800

    add example

---
+++ example.cpp
@@ -0,0 +1,56 @@
+#include <cmath>
+#include <sys/mman.h>
+
+#include "profiler.h"
+
+double cpu_heavy() {
+    PROFILE_FUNCTION();
+    // std::cout << "cpu_heavy\n";
+
+    double x{};
+    static int i{};
+
+    for (int c=0;c<100000;c++) {
+        i++;
+
+        x += sin(i) * cos(sqrt(i + 1));
+        x = fmod(x, 1000.0);
+    }
+
+    return x;
+}
+
+void syscall_heavy() {
+    PROFILE_FUNCTION();
+    // std::cout << "syscall_heavy\n";
+
+    timespec ts{};
+    for (int c=0;c<100000;c++) {
+        clock_gettime(CLOCK_MONOTONIC, &ts);
+    }
+}
+
+void mmap_heavy() {
+    PROFILE_FUNCTION();
+    // std::cout << "mmap_heavy\n";
+
+    const size_t sz = 4096 * 10;
+
+    for (int c=0;c<100000;c++) {
+        void* p = mmap(nullptr, sz, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+        if (p != MAP_FAILED) {
+            munmap(p, sz);
+        }
+    }
+}
+
+int main() {
+
+    while (true) {
+        cpu_heavy();
+        syscall_heavy();
+        mmap_heavy();
+    }
+
+    return 0;
+}
